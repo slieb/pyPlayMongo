32ca69a3daabcccd479737d1cb9a6d73bab4bd5f
Steven Lieblich <steven.lieblich@gmail.com>
2016-08-11 13:01:31 -0400

    Added exception logic / timeout for MongoDB connection

--- src/pyPlayMongo.py
+++ src/pyPlayMongo.py
@@ -1,14 +1,19 @@
 from pymongo import MongoClient
 
 # get a client handle to MongoDB
-client = MongoClient()
+try:
+    client = MongoClient(serverSelectionTimeoutMS=2000)
+    client.server_info()
+except Exception as e:
+    print "Connection failed:\n\t", e
+    exit (0)
 
 # use the test database
 db = client.test
 
 # set up variables for query parameters
 myCuisine = 'Korean'
-myBorough = 'Staten Island'
+myBorough = 'Manhattan'
 
 # get a cursor and iterate through the results
 cursor = db.restaurants.find({"cuisine": myCuisine, "borough": myBorough})
